80fa6e5eba7c7e19744296cdfc4420b944ddaa7d
adaschevici <a.daschevici@gmail.com>
2017-02-25 19:25:04 +0000

    Fix linting problems and update general layout

--- src/components/DonutChart/donut_chart_view.js
+++ src/components/DonutChart/donut_chart_view.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import * as d3 from 'd3';
 
 import Path from './path';
 import Legend from './legend';
 
-const DonutChartView = function (props) {
+const DonutChartView = (props) => {
+  const { id, width, height, updateData, data, chartType, padAngle } = props;
   const pie = d3.pie()
-    .value(function(d){ return d.count })
-    .padAngle(props.padAngle)
+    .value(d => d.count)
+    .padAngle(padAngle)
     .sort(null);
 
   const color = d3.scaleOrdinal()
-    .range(['#68c8d7','#eccd63','#bb8cdd','#de6942','#52b36e','#bbc7d9']);
+    .range(['#68c8d7', '#eccd63', '#bb8cdd', '#de6942', '#52b36e', '#bbc7d9']);
 
-  // tttt
   return (
     <div>
-      <svg id={props.id} width={props.width}
-        height={props.height} className="shadow" onClick={props.updateData}>
+      <svg
+        id={id}
+        width={width}
+        height={height}
+        className="shadow"
+        onClick={updateData}
+      >
 
-        <Path width={props.width} height={props.height}
-          pie={pie} color={color} data={props.data} chartType={props.chartType} />
+        <Path
+          width={width}
+          height={height}
+          pie={pie}
+          color={color}
+          data={data}
+          chartType={chartType}
+        />
 
-        <Legend pie={pie} color={color} data={props.data}
-          width={props.width} height={props.height} />
+        <Legend
+          pie={pie}
+          color={color}
+          data={data}
+          width={width}
+          height={height}
+        />
       </svg>
     </div>
   );
+};
+
+DonutChartView.propTypes = {
+  width: React.PropTypes.number.isRequired,
+  height: React.PropTypes.numbe.isRequired,
+  padAngle: React.PropTypes.number.isRequired,
+  id: React.PropTypes.string.isRequired,
+  chartType: React.PropTypes.string.isRequired,
+  updateData: React.PropTypes.func.isRequired,
+  data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
 };
 
 export default DonutChartView;
